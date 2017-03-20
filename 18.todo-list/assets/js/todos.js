d732f69495251292c9171bf7c1b94588d59146ff
Luca Celenza <celenzaluca@gmail.com>
2017-03-20 19:00:01 +0100

    Completed unit 18.

--- 18.todo-list/assets/js/todos.js
+++ 18.todo-list/assets/js/todos.js
@@ -18,8 +18,12 @@ $("input[type=\"text\"]").keypress(function(event) {
         //grabbing new todo text from input
         var todoText = $(this).val();
         //create a new li and add to ul
-        $("ul").append("<li><span>x</span> " + todoText + "</li>");
+        $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
         //reset input value to be empty
         $(this).val('');
     }
+});
+
+$("h1 i").click(function() {
+    $("input[type=\"text\"]").fadeToggle();
 });
