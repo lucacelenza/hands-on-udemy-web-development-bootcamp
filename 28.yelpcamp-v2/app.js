82bed4506306ecbc32492d6757d627430dc229dd
Luca Celenza <celenzaluca@gmail.com>
2017-03-31 06:28:54 +0000

    Completed unit 28

---
+++ 28.yelpcamp-v2/app.js
@@ -0,0 +1,74 @@
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+    
+mongoose.Promise = require("bluebird");
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+var Campground = mongoose.model("Campground", new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+}));
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", function(request, response) {
+    response.render("landing");
+});
+
+app.get("/campgrounds", function(request, response) {
+    Campground
+        .find({})
+        .then(function(campgrounds) {
+            var viewModel = {
+                campgrounds: campgrounds
+            };
+            
+            response.render("index", viewModel);
+        }, function(err) {
+            console.log(err);
+        });
+});
+
+app.get("/campgrounds/new", function(request, response) {
+    response.render("new");
+});
+
+//declare after "/campgrounds/new" to avoid route conflict
+app.get("/campgrounds/:id", function(request, response) {
+    Campground
+        .findById(request.params.id)
+        .then(function(campground) {
+            var viewModel = {
+                campground: campground
+            };
+            
+            response.render("show", viewModel);
+        }, function(err) {
+            console.log(err);
+        });
+});
+
+app.post("/campgrounds", function(request, response) {
+    var campground = {
+        name: request.body.name,
+        image: request.body.image,
+        description: request.body.description
+    };
+    
+    Campground
+        .create(campground)
+        .then(function() {
+            response.redirect("/campgrounds");
+        }, function(err) {
+            console.log(err);
+        });
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("YelpCamp server has started.");
+});
